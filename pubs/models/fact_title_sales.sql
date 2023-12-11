9084f8eae20d28259788a82e01e746a491222307
Data dockers <datadockers722@gmail.com>
2023-12-11 16:09:06 +0000

    go2

--- pubs/models/fact_title_sales.sql
+++ pubs/models/fact_title_sales.sql
@@ -14,13 +14,16 @@ stg_titles AS (
     SELECT * FROM {{ source("pubs", 'titles') }}
 ),
 stg_discounts AS (
-    SELECT * FROM {{ source("pubs", 'discounts') }}
+    SELECT *,
+    {{ dbt_utils.generate_surrogate_key(["discounttype"]) }} as discountkey
+    FROM {{ source("pubs", 'discounts') }}
 ),
 
 revenue_calc AS (
     SELECT 
         s.title_id,
-        s.titlekey, 
+        s.titlekey,
+        d.discountkey, 
         s.qty,
         COALESCE(d.discount, 0) as discount_percentage,
         (t.price - (t.price * (COALESCE(d.discount, 0))/100)) * s.qty AS revenue_per_title,
@@ -32,16 +35,18 @@ revenue_calc AS (
 total_revenue AS (
     SELECT 
         rc.title_id,
-        rc.titlekey, 
+        rc.titlekey,
+        rc.discountkey,
         SUM(rc.qty) AS total_qty,
         ROUND(SUM(rc.total_discount_per_sale),2) AS total_discount,
         ROUND(SUM(rc.revenue_per_title),2) AS total_revenue_per_title
     FROM 
        revenue_calc rc
-    GROUP BY rc.title_id,titlekey
+    GROUP BY rc.title_id,rc.titlekey,rc.discountkey
 )
 SELECT 
     tr.titlekey,
+    tr.discountkey,
     tr.title_id,
     tr.total_qty,
     tr.total_discount,
