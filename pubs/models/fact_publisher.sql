b1605c7789c1b730b0ac3e3428916a3d6d046a11
Data dockers <datadockers722@gmail.com>
2023-12-11 16:20:43 +0000

    gofinal

--- pubs/models/fact_publisher.sql
+++ pubs/models/fact_publisher.sql
@@ -18,15 +18,19 @@ title_count_per_publisher AS (
     SELECT 
         t.pub_id,
         p.publisherkey,
+        t.titlekey,
+        t.pubdate,
         COUNT(t.title_id) AS number_of_titles
     FROM stg_titles t
     JOIN stg_publishers p ON t.pub_id = p.pub_id
-    GROUP BY t.pub_id, p.publisherkey
+    GROUP BY t.pub_id, p.publisherkey, t.titlekey, t.pubdate
 )
 
 SELECT 
     tcp.pub_id,
     tcp.publisherkey,
+    tcp.titlekey,
+    tcp.pubdate,
     tcp.number_of_titles
 FROM title_count_per_publisher tcp
 ORDER BY tcp.pub_id, tcp.number_of_titles DESC
