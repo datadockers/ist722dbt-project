abcbb4fe2ba1f454f37fb462a9c0020bb6d694df
Data dockers <datadockers722@gmail.com>
2023-12-13 22:48:32 +0000

    gofinal

--- pubs/models/dim_roysched.sql
+++
@@ -1,8 +0,0 @@
-with stg_roysched as (select * from {{ source("pubs", "roysched") }})
-select
-    {{ dbt_utils.generate_surrogate_key(["r.title_id","r.royalty"]) }} as royschedkey,
-    r.title_id,
-    r.lorange,
-    r.hirange,
-    r.royalty
-from stg_roysched r
