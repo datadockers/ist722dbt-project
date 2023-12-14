7074fbbddcf2af9aba8435cbaeae56d21e8f8ba3
Data dockers <datadockers722@gmail.com>
2023-12-14 16:15:20 +0000

    gofinally

--- pubs/models/dim_titleauthor.sql
+++
@@ -1,8 +0,0 @@
-with stg_titleauthor as (select * from {{ source("pubs", "titleauthor") }})
-select
-    {{ dbt_utils.generate_surrogate_key(["ta.au_id"]) }} as titleauthorkey,
-    ta.au_id,
-    ta.title_id,
-    ta.au_ord,
-    ta.royaltyper
-from stg_titleauthor ta
